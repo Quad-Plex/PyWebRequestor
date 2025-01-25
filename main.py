ce6f86fcbce057c536909488ec510066a03fedcc
Quad-Plex <bastian-amann@outlook.de>
2025-01-25 17:41:42 +0100

    add exception logging

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import sys
+import traceback
+
 from PyQt5.QtWidgets import QApplication
+from PyQt5.uic.properties import QtWidgets
+
 from ui import WebRequestApp
 
+def excepthook(exc_type, exc_value, exc_tb):
+    tb = "".join(traceback.format_exception(exc_type, exc_value, exc_tb))
+    print("error caught!:")
+    print("error message:\n", tb)
+    QtWidgets.QApplication.quit()
+
 if __name__ == "__main__":
+    sys.excepthook = excepthook
     app = QApplication(sys.argv)
     window = WebRequestApp()
     window.show()
